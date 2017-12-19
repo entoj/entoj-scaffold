f4dde73f81f0b88e4d9621cae9a4086382488ccf
Christian Auth <c.auth@syzygy.de>
2017-12-19 14:39:13 +0100

    feat: adds module registration

--- source/index.js
+++ source/index.js
@@ -1,4 +1,32 @@
+'use strict';
+
+/**
+ * Registers with default configurations
+ *
+ * @param {Object} configuration
+ */
+function register(configuration, options)
+{
+    const opts = options || {};
+
+    // Commands
+    configuration.commands.add(require('./command/index.js').ScaffoldEntityCommand,
+        {
+            templatePath: opts.entityTemplatePath || '${data}/templates/entity'
+        });
+    configuration.commands.add(require('./command/index.js').ScaffoldPageCommand,
+        {
+            templatePath: opts.pageTemplatePath || '${data}/templates/page'
+        });
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
 module.exports =
 {
+    register: register,
     command: require('./command/index.js')
 };
